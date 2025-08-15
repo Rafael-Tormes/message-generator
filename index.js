aa1826fd2f8a22776f3a40556160dca944c4ecf7
Rafael Tormes <rafaeltormes00@gmail.com>
2025-08-15 18:33:32 -0500

    Add arrays and start function

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("Random Message Generator");
+const randMessages = {
+  first: ["I", "You", "They"],
+  second: [" smell", " seem", " are"],
+  third: [" stinky.", " gross.", " lovely."],
+};
+
+const randomNum = () => {
+  return Math.floor(Math.random() * 3);
+};
+
+const genMessage = (arr1, arr2, arr3) => {
+  let tempArr1 = arr1;
+  let tempArr2 = arr2;
+  let tempArr3 = arr3;
+
+  let firstWord = tempArr1[randomNum()];
+  let secondWord = tempArr2[randomNum()];
+  let thirdWord = tempArr3[randomNum()];
+  console.log(firstWord);
+  console.log(secondWord);
+  console.log(thirdWord);
+};
+
+genMessage(randMessages.first, randMessages.second, randMessages.third);
